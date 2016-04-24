0ca9f0c6e7a77f8f8457a68eb34ca85a4848264e
Alex Hirschfeld <alex@d-qoi.com>
2016-04-24 14:06:32 -0400

    It works

--- LapCounter.py
+++ LapCounter.py
@@ -7,10 +7,10 @@ See the read me for instructions
 There is no warrenty for this
 """
 import configparser
-import time
+import time as Time
+
 
 teamsFile = ""
-startTime = 0;
 distance = 0
 backup = ""
 
@@ -18,70 +18,91 @@ teamList = {}
 timeList = {}
 
 def timeConvert(time):
-	return time
+	return str(time)
 
 def readConfig():
+	global distance,teamsFile, backup
 	config = configparser.ConfigParser()
 	config.read('config.conf')
-	disntace = float(config["DEFAULT"]["DISTANCE"])
+	distance = float(config["DEFAULT"]["DISTANCE"])
 	teamsFile = config['DEFAULT']['TEAMS']
 	backup = config['DEFAULT']['BACKUP']
+	print(distance, teamsFile, backup)
 
 
 def readTeams():
+	global teamList, teamsFile
 	with open(teamsFile, 'r') as teams:
 		for line in teams:
 			data = line.strip().split(',')
 			teamList[data[0].strip()] = data[1].strip()
 			timeList[data[1].strip()] = []
 
-def start(time):
-	with open(backup, 'a') as backup:
+def startRace(time):
+	global backup
+	with open(backup, 'a') as back:
 		for teams in timeList.keys():
 			timeList[teams].append(time)
-			backup.write(teams + " " timeConvert(time))
+			back.write(teams + " = " + timeConvert(time) + "\n")
 
 def calcRank():
+	global teamList, distance, timeList
 	toSort = {}
 	for team in teamList.keys():
-		times = timeList[team]
-		dist = len(times) * disntace
-		if len(times) == 0:
+		times = timeList[teamList[team]]
+		dist = len(times) * distance
+		if len(times) < 2:
 			continue
 		speed = dist/(times[-1] - times[0])
 		toSort[team] = speed
-	return sorted(toSort, key=lambda split:split[1])
+	toSort = sorted(toSort, key=lambda split:split[1])
+	toSort.reverse()
+	return toSort
 
 def main():
+	global teamList, timeList, backup
 	run = True
 	start = False
 	while run:
-		userIn = input(:)
-		if userIn.contains("start") and not start:
+		userIn = input(":").lower()
+		if "start" in userIn and not start:
 			start = True
-			start(time.time())
+			startRace(Time.time())
 			continue
 
-		elif userIn.contains("list"):
+		elif "list" in userIn:
+			#print(teamList.keys())
 			num = userIn.strip("list").strip()
-			for t in timeList[num]:
-				print(t)
-			continue
+			try:
+				for t in timeList[num]:
+					print(t)
+				print(len(timeList[num]), 'laps')
+			except:
+				print("not a team")
 
-		elif userIn.contains("rank"):
+		elif "rank" in userIn:
 			for rank in calcRank():
 				print(rank)
+
 			continue
 
-		elif userIn.contains("end"):
+		elif "end" in userIn:
 			run = False
 			continue
 
 		else:
-			tm = time.time()
-			nums = userIn.split(' ').strip()
-			for num in nums:
-				timeList[num.strip].append(tm)
+			if not start:
+				print("Race not Started")
+				continue
+			with open(backup,'a') as back:
+				tm = Time.time()
+				nums = userIn.split(' ')
+				for num in nums:
+					if num in timeList:
+						timeList[num.strip()].append(tm)
+						back.write(num + ' = ' + timeConvert(tm) + '\n')
+					else:
+						print(num,"is not a declared team")
 			continue
 
 if __name__ == "__main__":
